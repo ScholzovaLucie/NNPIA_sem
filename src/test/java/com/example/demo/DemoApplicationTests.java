4b4a7b6d59ca1923e04a93a2e7277415a02e1756
Scholzova <scholzova@stocked.cz>
2024-03-25 08:56:44 +0100

    first commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.semestralni_prace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
